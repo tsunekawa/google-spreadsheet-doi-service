530d1149298b4cdd42d59acffef5d2c77fa5d895
Tsunekawa Mao <tkunimiya@gmail.com>
2020-01-27 16:52:09 +0000

    add test cases for methods of DOI

--- src/test/doi.test.ts
+++ src/test/doi.test.ts
@@ -12,17 +12,29 @@ declare function GasTap(): test
 function unitTestDOIClass() {
   let test = GasTap()
   const DOI = Crossref.DOI
+  let doi = "https://doi.org/10.7191/jeslib.2014.1060"
 
-  test("DOI#parse", (t) => {
+  test("DOI#new", (t) => {
     t.notThrow(() => {
       new DOI("10.7191", "jeslib.2014.1060")
     }, "DOI can create an instance")
+  })
 
-    let doi = "https://doi.org/10.7191/jeslib.2014.1060"
+  test("DOI#parse", (t) => {
     let parsed = DOI.parse(doi)
     t.equal(parsed.prefix, "10.7191", "prefix of " + doi + " is 10.7191")
     t.equal(parsed.suffix, "jeslib.2014.1060", "suffix of " + doi + " is jeslib.2014.1060")
+  })
+
+  test("DOI#toString", (t) => {
+    let parsed = DOI.parse(doi)
+
     t.equal(parsed.toString(), "10.7191/jeslib.2014.1060", "DOI Name of " + doi + " is 10.7191/jeslib.2014.1060")
+  })
+
+  test("DOI#toURI", (t) => {
+    let parsed = new DOI("10.7191", "jeslib.2014.1060")
+    t.equal(parsed.toURI(), doi, "DOI URI of " + doi + "is " + doi)
   })
 
 }
