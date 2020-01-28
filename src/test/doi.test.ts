30556046ca63ef7e8233a888bac9c1401abb6cfe
Tsunekawa Mao <tkunimiya@gmail.com>
2020-01-29 00:37:55 +0900

    add test cases for DOI#equal

--- src/test/doi.test.ts
+++ src/test/doi.test.ts
@@ -26,6 +26,19 @@ function unitTestDOIClass() {
     t.equal(parsed.suffix, "jeslib.2014.1060", "suffix of " + doi + " is jeslib.2014.1060")
   })
 
+  test("DOI#equal", (t) => {
+    let doi_a = new DOI("10.7191", "jeslib.2014.1060")
+    let doi_b = new DOI("10.7191", "jeslib.2014.1060")
+    let doi_c = new DOI("10.7191", "JESLIB.2014.1060")
+    let doi_d = new DOI("10.7191", "jeslib.２０１４.1060")
+
+    t.ok(doi_a.equal(doi_b), "When two DOI instances have same prefix and suffix, equeal method always return true")
+    t.ok(doi_b.equal(doi_a), "When two DOI instances have same prefix and suffix, equeal method always return true")
+    t.ok(doi_a.equal(doi_c), "DOI name is not case-sensitive")
+    t.ok(doi_c.equal(doi_a), "DOI name is not case-sensitive")
+    t.notOk(doi_a.equal(doi_d), "Half-width and Full-width characters in DOI Names should be distinguished")
+  })
+
   test("DOI#toString", (t) => {
     let parsed = DOI.parse(doi)
 
